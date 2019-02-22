f01feb5d1a222dcec63364b57f6a5531bb225ef4
kadegnon <kodjo.adegnon@student.unamur.be>
2019-02-23 00:17:32 +0100

    Add backend

---
+++ backend/src/main/java/be/unamur/info/mdl/groggy/LaboMdLApplication.java
@@ -0,0 +1,13 @@
+package be.unamur.info.mdl.groggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboMdLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboMdLApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/be/unamur/info/mdl/groggy/LaboMdLApplicationTests.java
@@ -0,0 +1,16 @@
+package be.unamur.info.mdl.groggy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LaboMdLApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
